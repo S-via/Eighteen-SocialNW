d7ac36626bcaeb3bf8c85f8d0da93fddfba2d897
S-via <168160925+S-via@users.noreply.github.com>
2024-11-02 00:22:04 -0700

    added logic into userContorllers.js

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,11 +1,10 @@
-
-
 // require models User & Though from models folder
-const { User, Thought } = require('../models')
+const { User} = require('../models')
 
 module.exports = {
+    //// api/users ////
 
-    //get a user
+    // get users
     async getUsers(req, res) {
         try {
             const users = await User.find({});
@@ -75,7 +74,49 @@ module.exports = {
         } catch (err) {
             res.status(500).json(err)
         }
-    }
+    },
+    //// /api/users/:userId/friends/friendId ////
+
+    // post add new friend to users friend list
+    async addNewFriend(req, res) {
+        try {
+            const newFriend = await User.findOneAndUpdate(
+                { _id: req.params.userId },
+                { $addToSet: { friends: req.body } },
+                { runValidators: true }
+            );
+
+            if (!newFriend) {
+                return res
+                    .status(404)
+                    .json({ message: 'no friend found' })
+            }
+            res.json(newFriend);
+
+        } catch (err) {
+            res.status(500).json(err);
+        }
+    },
+
+    // delete remove friend from user's friend list 
+    async deleteFriend(req, res) {
+        try {
+            const deletedFriend = await User.findOneAndUpdate(
+                { _id: req.params.userId },
+                { $pull: { friends: { friendsId: req.params.friendsId } } },
+                { runValidators: true, new: true }
+            );
+            if (!deletedFriend) {
+                return res
+                    .status(404)
+                    .json({ message: 'no friend found' })
+            }
+            res.json(deletedFriend);
+
+        } catch (err) {
+            res.status(500).json(err);
+        }
+    },
 
 
-}
+};
